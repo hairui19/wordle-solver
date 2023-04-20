6276ffeb88e6b7c0f365a295eb58696f53d9cd47
hairuilin <linhairui19@gmail.com>
2023-04-20 18:40:17 +0800

    complete solver APIs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 use wordle_solver::{Guess, MatchResult, Solver};
 
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+
 fn main() {
     use MatchResult::*;
     let guess = Guess::new(
@@ -11,8 +13,23 @@ fn main() {
     );
 
     let dictionary: &str = include_str!("../dictionary.txt");
-    let all_words = Vec::from_iter(dictionary.lines().map(| line | line.split_once(" ").unwrap().0)); 
+    let all_words = Vec::from_iter(
+        dictionary
+            .lines()
+            .map(|line| line.split_once(" ").unwrap().0),
+    );
     let mut hash_map = HashMap::<&'static str, f64>::new();
     let mut solver = Solver::new(all_words);
-    println!("{:?}", solver.suggest_top_ten_words()); 
+
+    println!("{}", CLEAR);
+    for suggested_word in solver.suggest_top_ten_words() {
+        println!("{}-{}", suggested_word.0, suggested_word.1);
+    }
+
+    let mut buffer = String::new();
+    loop {
+        buffer.clear();
+        std::io::stdin().read_line(&mut buffer).unwrap();
+        println!("{}", buffer.trim());
+    }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,14 +1,17 @@
 use crate::Guess;
 use crate::MatchResult;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt;
-use std::collections::HashMap; 
+use std::fs::File;
 
 const DICTIONARY: &str = include_str!("../dictionary.txt");
+const FIRST_GUESSES_FILE_PATH: &str = "first_guesses_file.json";
 
 pub struct Solver {
     remaining_words: Vec<&'static str>,
     match_combinations: Vec<[MatchResult; 5]>,
+    is_first_guess: bool,
 }
 
 impl fmt::Debug for Solver {
@@ -30,29 +33,61 @@ impl Solver {
         Solver {
             remaining_words: words,
             match_combinations: MatchResult::get_cartesian_product().collect::<Vec<_>>(),
+            is_first_guess: true,
         }
     }
 
-    pub fn suggest_top_ten_words(&self) -> Vec<&'static str> {
-        let entropies_map = self.calculate_entropies_for_remaining_words();
-        let mut vec = entropies_map.into_iter().collect::<Vec<_>>();
-        vec.sort_by(|(_, p_1), (_, p_2)| p_1.partial_cmp(p_2).unwrap_or(std::cmp::Ordering::Equal));
-      
-        return vec.into_iter().take(10).map(|(word, _)| word).collect();
+    pub fn suggest_top_ten_words(&self) -> Vec<(String, f64)> {
+        if self.is_first_guess {
+            let file = File::open(FIRST_GUESSES_FILE_PATH).unwrap();
+            let entropies_map: HashMap<String, f64> = serde_json::from_reader(file).unwrap();
+            let mut vec = entropies_map.into_iter().collect::<Vec<_>>();
+            vec.sort_by(|(_, p_1), (_, p_2)| {
+                p_2.partial_cmp(p_1).unwrap_or(std::cmp::Ordering::Equal)
+            });
+
+            return vec.into_iter().take(10).collect();
+        } else {
+            let entropies_map = self.calculate_entropies_for_remaining_words();
+            let mut vec = entropies_map.into_iter().collect::<Vec<_>>();
+            vec.sort_by(|(_, p_1), (_, p_2)| {
+                p_2.partial_cmp(p_1).unwrap_or(std::cmp::Ordering::Equal)
+            });
+
+            return vec
+                .into_iter()
+                .take(10)
+                .map(|(word, entropy)| (word.to_string(), entropy))
+                .collect();
+        }
     }
 
-    fn calculate_entropies_for_remaining_words(&self) -> HashMap::<&'static str, f64> {
+    pub fn calibrate_on_guess(&mut self, guess: &Guess) {
+        let guess_word = &guess.get_word();
+        let guess_result = guess.get_result();
+        self.remaining_words
+            .retain(|word| Self::is_matching(word, guess_word, guess_result));
+    }
+
+    fn calibrate(&self) {
+        let hash_map = self.calculate_entropies_for_remaining_words();
+        let file = File::create(FIRST_GUESSES_FILE_PATH).unwrap();
+        serde_json::to_writer_pretty(file, &hash_map);
+    }
+
+    fn calculate_entropies_for_remaining_words(&self) -> HashMap<&'static str, f64> {
         let mut entropies_map = HashMap::<&'static str, f64>::new();
         for word in self.remaining_words.iter() {
-            let word_entropy = self.calculate_entropy_for_word(word); 
+            let word_entropy = self.calculate_entropy_for_word(word);
             entropies_map.entry(word).or_insert(word_entropy);
         }
-        return entropies_map; 
+        return entropies_map;
     }
 
     /// Calculates the entropy value of the input `guess_word` against
     /// the remaining set of words in the solver.
     fn calculate_entropy_for_word(&self, guess_word: &str) -> f64 {
+        println!("Calibrating word: {}", guess_word);
         let mut entropy = 0.0;
         let total_remaining_word_count = self.remaining_words.len();
         for match_combination in self.match_combinations.iter() {
